212255959f20753a733dfb20c9607a5686891fbd
RedBerserk <nosbit98@mail.ru>
2023-02-14 13:41:59 +0300

    feat: solution is ready

--- src/index.js
+++ src/index.js
@@ -35,10 +35,66 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    ' ': ' ',
 };
 
-function decode(expr) {
-    // write your solution here
+function decode(expr){
+    let arr = [];
+    let brr = [];
+    let crr = [];
+    let count = 0;
+    for (let i = 0; i < expr.length; i++){
+        arr.push(expr[i]);
+        count++;
+        if (count == 10 || i == expr.length - 1){
+            brr.push(arr.join(''));
+            arr = [];
+            count = 0;
+        }
+    }
+    for (let i = 0; i < brr.length; i++){
+        if(brr[i].length == 10){
+            crr.push(morse(brr[i]));
+        }else if(brr[i].length < 10){
+            crr.push(morse('0'.repeat(10 - brr[i].length) + brr[i]));
+        }
+    }
+    let string = '';
+    for (let i = 0 ; i < crr.length; i++){
+        for(let key in MORSE_TABLE){
+            if(crr[i] == key){
+                string += MORSE_TABLE[key];
+            }
+        }
+    }
+    return string;
+}
+
+function morse(str){
+    if (str == '**********') return ' ';
+    let arr = [];
+    let brr = [];
+    let res = '';
+    let count = 0;
+    for (let i = 0; i < str.length; i++){
+        arr.push(str[i]);
+        count++;
+        if (count == 2){
+            let s = arr.join('');
+            if (s == '00'){
+                res += '';
+            }else if (s == '10'){
+                res += '.';
+            }else{
+                res += '-';
+            }
+            brr.push(res);
+            res = '';
+            count = 0;
+            arr = [];
+        }
+    }
+    return brr.join('');
 }
 
 module.exports = {
